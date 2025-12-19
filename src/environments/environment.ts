714c1c583cc80917f15b82649fc4dd3c569f2514
emiliano965 <druettaemiliano@gmail.com>
2025-12-19 01:41:25 -0300

    refactor apiBase: 'http://localhost:3000'

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBase: 'http://3.15.176.201:3000'
+  apiBase: 'http://localhost:3000'
 };
 
 // Cambiar a true y ajustar el "apiBase" si se quiere development según el entorno real
